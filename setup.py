e5bef31fa042ca70659e4e15edd0a9123620f3a2
carlio <git@carlcrowder.com>
2012-06-24 08:30:24 +0200

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+import time
+
+
+_version = "0.%s.dev" % int(time.time())
+_packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+    
+# common dependencies
+_install_requires = [
+            'django',
+            'django-annoying',
+            'south',
+            'celery',
+       ]
+
+setup( name='django-scraping',
+       version=_version,
+       packages=_packages,
+       install_requires=_install_requires,
+       scripts=[
+           # 'scripts/manage',
+       ],
+)
