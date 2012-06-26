d9700f141eb8641b0e34431e6a5cda0b49397bf0
Carl Crowder <github@carlcrowder.com>
2012-06-26 18:53:50 +0200

    Adding settings for unit tests

---
+++ scraping/tests/settings.py
@@ -0,0 +1,12 @@
+
+DATABASES = {
+        'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+        }
+}
+
