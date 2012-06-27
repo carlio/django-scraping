d8af474b0394d58af3819c72091e9f78875c3c81
carlio <git@carlcrowder.com>
2012-06-27 21:05:11 +0200

    Adding dummy test to prevent jenkins moaning

---
+++ scraping/tests/test_test.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+class TestForTheSakeOfIt(TestCase):
+    """
+    Non-event test just to stop jenkins moaning about 0 tests
+    """
+    
+    def test_nothing(self):
+        pass
