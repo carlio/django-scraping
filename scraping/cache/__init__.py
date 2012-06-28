ed5f9b7b80cb149a980fa0284d1d3b335d9f421f
carlio <git@carlcrowder.com>
2012-06-28 10:22:46 +0200

    Adding a default cache module for situations when the settings module does not specify one

--- scraping/cache/__init__.py
+++ scraping/cache/__init__.py
@@ -1,4 +1,5 @@
 from django.utils.importlib import import_module
 from django.conf import settings
 
-cache = import_module(settings.SCRAPER_CACHE_MODULE)
+cache_module = getattr(settings, 'SCRAPER_CACHE_MODULE', 'scraping.cache.dummy')
+cache = import_module(cache_module)
