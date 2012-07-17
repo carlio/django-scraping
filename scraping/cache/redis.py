b27677b0993bb85f8c9f72c37ba67c11fbc373df
Carl Crowder <github@carlcrowder.com>
2012-07-17 11:03:43 +0200

    Fixing redis cache to properly store JSON encoded data

--- scraping/cache/redis.py
+++ scraping/cache/redis.py
@@ -1,9 +1,12 @@
+from __future__ import absolute_import # required to avoid the name clash below with the redis client
+
 from scraping.cache.base import ScraperCacheBase
 from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
+import json
 
 try:
-    import redis
+    import redis as redis_client
 except ImportError:
     raise ImproperlyConfigured('The "redis" python client package is required to use Redis as a cache - get it here http://pypi.python.org/pypi/redis/')
 
@@ -14,7 +17,7 @@ class RedisCache(ScraperCacheBase):
                  'port': getattr(settings, 'SCRAPER_CACHE_REDIS_PORT', 6379),
                  'password': getattr(settings, 'SCRAPER_CACHE_REDIS_PASSWORD', None),
                  'db': settings.SCRAPER_CACHE_REDIS_DB }
-        return redis.Redis(**info)
+        return redis_client.Redis(**info)
 
     def _key(self, url):
         return 'url:%s' % url 
@@ -37,7 +40,10 @@ class RedisCache(ScraperCacheBase):
         db = self._get_db()
         
         data = db.get(url)
-        contents, real_url = data or (None, None)
+        if data:
+            contents, real_url = json.loads(data)
+        else:
+            contents, real_url = (None, None)
         
         if contents is not None:
             self._inc_hit_count()
@@ -46,7 +52,7 @@ class RedisCache(ScraperCacheBase):
     
     def put_contents(self, url, contents, real_url):
         db = self._get_db()
-        db.set(url, (contents, real_url) )
+        db.set(url, json.dumps((contents, real_url)) )
     
     
     def get_size(self):
