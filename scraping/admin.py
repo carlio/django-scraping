3b42aeb1d09758bc5bd62880710dbf779cf75b1f
carlio <git@carlcrowder.com>
2012-07-21 09:00:29 +0200

    Making scrape attempts readonly in the admin

--- scraping/admin.py
+++ scraping/admin.py
@@ -27,6 +27,7 @@ class ScrapeAttemptAdmin(admin.ModelAdmin):
     search_fields = ('page__url',)
     list_filter = ('started', 'completed', 'state')
     list_display = ('get_page_url', 'started', 'completed', 'state', 'time_taken')
+    readonly_fields = ('page', 'state', 'completed', 'error_message')
     actions = [reschedule_scrape_attempt]
 
 admin.site.register(ScrapeAttempt, ScrapeAttemptAdmin)
